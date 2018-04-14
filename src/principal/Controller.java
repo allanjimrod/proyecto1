4873b8b36f0b25073a778e48d54a5d068269a8bd
allanjimrod <allan.jimrod@gmail.com>
2018-04-14 17:58:44 -0600

    Ejemplo Java FX

--- src/principal/Controller.java
+++ src/principal/Controller.java
@@ -1,4 +1,21 @@
 package principal;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
 public class Controller {
+    @FXML
+    private Button botonPrueba;
+
+    @FXML
+    private TextArea textoPrueba;
+
+
+    @FXML
+    private void clickEnBotonPrueba(ActionEvent actionEvent) {
+        System.out.print("Hizo click en el fucking button!");
+    }
 }
